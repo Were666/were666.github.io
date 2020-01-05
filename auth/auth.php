df3e060fb7a382f71911258fe71a45eb84a1fa5d
Were Pointe du Lac <were.666@gmail.com>
2020-01-05 22:34:17 +0100

    Testing Auths

--- auth/auth.php
+++ auth/auth.php
@@ -8,12 +8,13 @@
 
 	$cliente = curl_init();
     curl_setopt($cliente, CURLOPT_URL, "https://api.instagram.com/oauth/access_token");
-    curl_setopt($cliente, CURLOPT_POST, 1);
-    curl_setopt($cliente, CURLOPT_POSTFIELDS, "app_id=".$app_id);
+    curl_setopt($cliente, CURLOPT_POST, true);
+    /*curl_setopt($cliente, CURLOPT_POSTFIELDS, "app_id=".$app_id);
     curl_setopt($cliente, CURLOPT_POSTFIELDS, "app_secret=".$app_secret);
     curl_setopt($cliente, CURLOPT_POSTFIELDS, "grant_type=authorization_code");
     curl_setopt($cliente, CURLOPT_POSTFIELDS, "redirect_uri=".$redirect_url);
-    //curl_setopt($cliente, CURLOPT_POSTFIELDS, $data);
+    */
+    curl_setopt($cliente, CURLOPT_POSTFIELDS, $data);
     //curl_setopt($cliente, CURLOPT_CUSTOMREQUEST, "POST");
 
 	//curl_setopt($cliente, CURLOPT_HEADER, 0);
@@ -22,7 +23,7 @@
 	$contenido = curl_exec($cliente);
     curl_close($cliente);
     
-    echo $contenido;
+    return $contenido;
 
     //curl -X POST \ https://api.instagram.com/oauth/access_token \ -F app_id={app-id} \ -F app_secret={app-secret} \ -F grant_type=authorization_code \ -F redirect_uri={redirect-uri} \ -F code={code}
 ?>
