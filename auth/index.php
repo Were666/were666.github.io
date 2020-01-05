57dff97e668f8a36dc06dadcc717fd928ad92ff8
Were Pointe du Lac <were.666@gmail.com>
2020-01-05 13:18:39 +0100

    Testing Auths

---
+++ auth/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="Were Pointe du lac">
+  
+        <link rel="shortcut icon" href="../favicon.ico">
+        <link rel="apple-touch-icon" href="../favicon.ico">
+
+        <title>O.Redes</title>
+        <meta http-equiv="Refresh" content="0;url=https://api.instagram.com/oauth/authorize?app_id=530986217627069&redirect_uri=https://were666.github.io/auth/&scope=user_profile,user_media&response_type=code">
+    </head>
+</html>
